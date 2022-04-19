1c1cf64ba1bfcea97328f9f7905016923fb6adda
RoyCal <vito.elias9@gmail.com>
2022-04-19 11:18:51 -0300

    ---> dados.txt

--- Arquivo.cpp
+++ Arquivo.cpp
@@ -3,7 +3,8 @@
 void Arquivo::imprimeArquivo(){
     int i;
 
-    arquivo.open("funcionarios.txt", ios::in);
+    arquivo.open("dados
+    .txt", ios::in);
     
     i = 0;                              //Colocando todas as linhas do arquivo em um vetor de strings
     while(getline(arquivo, linhas[i])){
@@ -20,10 +21,12 @@ void Arquivo::imprimeArquivo(){
 }
 
 void Arquivo::criaArquivo(){
-    arquivo.open("funcionarios.txt", ios::in);
+    arquivo.open("dados
+    .txt", ios::in);
 
     if(arquivo.is_open() == false){                 ///////////////////////////////////////////////
-        arquivo.open("funcionarios.txt", ios::out); //se o arquivo nao existe, ele cria o arquivo//
+        arquivo.open("dados
+        .txt", ios::out); //se o arquivo nao existe, ele cria o arquivo//
         arquivo.close();                            ///////////////////////////////////////////////
     }
 
